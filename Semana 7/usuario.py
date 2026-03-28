e843462052b4a9a5172e41de6b97748640e685f6
Sebaspmican <felipemican28@gmail.com>
2026-03-27 21:10:57 -0500

    Create usuario.py

---
+++ Semana 7/usuario.py
@@ -0,0 +1,7 @@
+usuario =input("escribe tu usuario")
+contrasena = input("escribe la contraseña")
+if usuario == "admin" and contrasena == "1234":
+    print("Acceso concedido. Bienvenido")
+    print("cargado tu perfir")
+else:
+    print("acceso denegado.Datos incorrectos.")
